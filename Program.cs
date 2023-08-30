6b870dfdc639fd7830cc91173cb0b346fb0e7889
Aleokras <Aleokras@DESKTOP-PKGPB2Q>
2023-08-30 20:11:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+ConverterHourToMinutesAndSeconds(77.5);
+ConverterMeterToСentimetersAndMillimeters(44.560);
+
+
+
+void ConverterHourToMinutesAndSeconds(double hour)
+{
+    Console.WriteLine($"{hour * 60}: minutes");
+    Console.WriteLine($"{hour * 3600}: seconds");
+}
+
+
+void ConverterMeterToСentimetersAndMillimeters(double meter)
+
+{
+    Console.WriteLine();
+    Console.WriteLine($"{meter * 100}: centimeters");
+    Console.WriteLine($"{meter * 1000}: millimeters");
+}
